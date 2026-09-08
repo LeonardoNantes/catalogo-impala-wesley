fcf29857be541ed126cbbe570e9a847154fb9b46
LeonardoNantes <leonardonantes@outlook.com>
2026-09-08 18:22:52 -0300

    Update vendor name from 'Wesley3' to 'Wesley'

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ const CONFIG = {
 
   // ---- Dados do vendedor (aparecem no cabeçalho e no link do WhatsApp) ----
   vendedor: {
-    nome: "Wesley3",
+    nome: "Wesley",
     slogan: "O seu Vendedor!",
     foto: "assets/vendedor-foto.jpg",
     // Número de WhatsApp no formato internacional, só números (DDI 55 + DDD + número)
